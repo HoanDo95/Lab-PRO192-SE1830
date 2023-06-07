0c067984bbc2d25bf52ebe46826ca619fee5b616
HoanDo95 <hoandshe170447@fpt.edu.vn>
2023-06-07 14:51:15 +0700

    update WS4

---
+++ workshop4/src/Menu/Menu.java
@@ -0,0 +1,18 @@
+
+package Menu;
+
+import java.util.Scanner;
+
+
+public class Menu {
+    public static int getChoice(Object[] options) {
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + "-" + options[i]);
+        }
+
+        System.out.println("Choice 1 .." + options.length + ": ");
+
+        Scanner sc = new Scanner(System.in);
+        return Integer.parseInt(sc.nextLine());
+    }
+}
